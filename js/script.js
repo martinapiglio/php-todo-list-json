d83a50111c11be9a4ed9f313a4e384768e2befe9
martinapiglio <martina.piglio@gmail.com>
2023-04-20 15:42:37 +0200

    'add to do' function (not from json data)

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@ const { createApp } = Vue
 createApp({
 	data() {
 		return {
-            todos: []
+            todos: [],
+            newToDo: '',
 		}
 	},
     methods:{
+
         getToDos() {
             axios.get('./server.php').then((res) => {
                 this.todos = res.data;
@@ -14,8 +16,30 @@ createApp({
             .catch((err) => {
                 this.todos = [];
             })
+        },
+
+        addToDo() {
+      
+            let newToDoObject = {
+                text: this.newToDo,
+                done: false,
+            };
+
+            this.todos.push(newToDoObject);
+      
+            // axios.post('./server.php', newToDoObject, {headers: {'Content-Type': 'multipart/form-data'}}).then(res => {
+            //     this.getToDos();
+            // });
+
+            this.newToDo = '';
+        },
+
+        changeStatus(element) {
+            element.done = !element.done;
         }
+
     },
+    
     mounted() {
         this.getToDos();
     }
